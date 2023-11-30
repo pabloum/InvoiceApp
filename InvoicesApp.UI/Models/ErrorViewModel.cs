c592666a99656155fc39d1b382e640fd1185f3b8
Pablo Uribe <pablo.um00@gmail.com>
2023-11-29 20:31:24 -0500

    Create solution

---
+++ InvoicesApp.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InvoicesApp.UI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
